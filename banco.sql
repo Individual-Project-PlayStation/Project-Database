d1edb39e5f6f4ab158d2fd0da4615552e238142f
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-06-03 17:32:09 -0300

    Banco de Dados 100% Finalizado!

---
+++ banco.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE projetoIndividual;
+USE projetoIndividual;
+
+
+CREATE TABLE tbUsuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(200)NOT NULL UNIQUE,
+    senha VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE tbQuiz (
+	idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+    dataInicio DATETIME NOT NULL
+);
+
+CREATE TABLE tbMetrica (
+	idMetrica INT AUTO_INCREMENT, 
+    fkUsuario INT NOT NULL, CONSTRAINT metricaUsuario FOREIGN KEY (fkUsuario) REFERENCES tbUsuario (idUsuario),
+    fkQuiz INT NOT NULL, CONSTRAINT metricaQuiz FOREIGN KEY (fkQuiz) REFERENCES tbQuiz (idQuiz),
+    acertos INT NOT NULL,
+    erros INT NOT NULL,
+    PRIMARY KEY (idMetrica, fkUsuario, fkQuiz)
+);
+
+-- -----------------------------------SELECT PADRÃO
+
+SELECT * FROM tbUsuario;
+SELECT * FROM tbQuiz;
+SELECT * FROM tbMetrica;
+
+-- -----------------------------------SELECTS DA API
+
+-- RANKING
+SELECT tbUsuario.nome, erros, acertos FROM tbMetrica 
+	join tbUsuario 
+		on idUsuario = fkUsuario	
+			ORDER BY acertos DESC;
+   
+   
+-- PROGRESSO            
+            
+SELECT acertos FROM tbMetrica WHERE fkUsuario = 1 ORDER BY acertos DESC LIMIT 7;
+
+
+-- RESULTADO QUIZ
+            
+SELECT * FROM tbMetrica WHERE fkUsuario = 1 and fkQuiz = (SELECT MAX(fkQuiz) FROM tbMetrica WHERE fkUsuario = 1);
